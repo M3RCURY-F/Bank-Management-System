f7dded2e2f3976f88483fa7ec2b474e2cd4d9269
M3RCURY-F <m3rcuryf@gmail.com>
2026-06-13 09:35:21 +0400

    transactions.h

--- Bank_Management/transaction.h
+++ Bank_Management/transaction.h
@@ -5,3 +5,4 @@ bool addBalance(Account accounts[], int accountCount, int accountId, int userId,
 bool withdraw(Account accounts[], int accountCount, int accountId, int userId, double amount);
 bool transfer(Account accounts[], int accountCount, int fromAccountId, int toAccountId, int userId, double amount);
 bool payment(Account accounts[], int accountCount, int accountId, int userId, double amount);
+void showBalance(Account accounts[], int accountCount, int accountId, int userId);
